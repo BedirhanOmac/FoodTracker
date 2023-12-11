c48ab51b8ece8ca183b8a512183e961f8fecdd94
engincanoz <ozengincan424@gmail.com>
2023-12-11 22:31:24 +0300

    some xml fixes

---
+++ app/src/main/java/com/example/foodtrackerv1012/set.java
@@ -0,0 +1,12 @@
+package com.example.foodtrackerv1012;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class set extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
